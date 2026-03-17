5a0bfc4767c8407eed7bcb594c33791db5b2565b
ParthOmneNest <parth.das@omnenest.com>
2026-03-17 15:29:21 +0530

    added the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useAuthStore } from "../store/useAuthStore"
+import logo from '../assets/OmneNest_icon.png'
 
 export const HomePage = () => {
     const { logout } = useAuthStore();
@@ -7,14 +8,25 @@ export const HomePage = () => {
         logout();
     }
     return (
-        <div>
-            <h1>HomePage</h1>
-            <button
-                onClick={handleLogout}
-                className="px-6 py-2 mt-4 bg-[#FAEBE9] text-[#CA3521] font-medium rounded-lg transition-colors hover:bg-[#f4d6d3] cursor-pointer"
-            >
-                Logout
-            </button>
+        <div className="flex items-center justify-center min-h-screen bg-white font-inter gap-3">
+            <div className="flex flex-col items-center gap-6">
+                
+                
+                <div className="flex items-center">
+                    <img src={logo} alt="Logo" className="w-20 h-10 object-contain -mr-10" />
+                    <span className="text-2xl font-bold text-[#464646]">NT Web</span>
+                </div>
+
+                <h1 className="text-medium font-semibold text-[#2A2A2B]">Dashboard</h1>
+
+                <button
+                    onClick={() => logout()}
+                    className="px-6 py-2 bg-[#FAEBE9] text-[#CA3521] font-semibold rounded-lg hover:bg-[#f4d6d3] transition-colors cursor-pointer"
+                >
+                    Logout
+                </button>
+                
+            </div>
         </div>
-    )
+    );
 }
